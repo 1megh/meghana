56067d5758816dc4b29f71e20ea2d2c0225bdc5f
1megh <meghana24kc@gmail.com>
2023-12-06 23:45:16 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Layout from './components/shared/Layout';
+import Dashboard from './components/Dashboard';
+import Products from './components/Products';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+        <Route path="login" element={<div>login page</div>}/>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
